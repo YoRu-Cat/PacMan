c41c389cd4a3408fe1fde088d2e300b55cbb3d15
YoRu Tech <tech.niko.coding@gmail.com>
2024-03-08 14:27:39 +0500

    Moving Pacman

--- src/Pacman.js
+++ src/Pacman.js
@@ -1,3 +1,4 @@
+import MovingDirection from "./MovingDirection.js";
 export default class Pacman {
   constructor(x, y, tileSize, velocity, tileMap) {
     this.x = x;
@@ -5,9 +6,13 @@ export default class Pacman {
     this.tileSize = tileSize;
     this.velocity = velocity;
     this.tileMap = tileMap;
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+    document.addEventListener("keydown", this.#keydown);
     this.#loadPacmanImages();
   }
   draw(ctx) {
+    this.#move();
     ctx.drawImage(
       this.pacmanImages[this.pacmanImageIndex],
       this.x,
@@ -36,5 +41,56 @@ export default class Pacman {
       pacmanImage4,
     ];
     this.pacmanImageIndex = 0;
+  }
+
+  #keydown = (event) => {
+    // up
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down)
+        this.currentMovingDirection = MovingDirection.up;
+      this.requestedMovingDirection = MovingDirection.up;
+    }
+    // down
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up)
+        this.currentMovingDirection = MovingDirection.down;
+      this.requestedMovingDirection = MovingDirection.down;
+    }
+    // left
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right)
+        this.currentMovingDirection = MovingDirection.left;
+      this.requestedMovingDirection = MovingDirection.left;
+    }
+    // right
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left)
+        this.currentMovingDirection = MovingDirection.right;
+      this.requestedMovingDirection = MovingDirection.right;
+    }
+  };
+  #move() {
+    if (this.currentMovingDirection !== this.requestedMovingDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        this.currentMovingDirection = this.requestedMovingDirection;
+      }
+    }
+    switch (this.currentMovingDirection) {
+      case MovingDirection.up:
+        this.y -= this.velocity;
+        break;
+      case MovingDirection.down:
+        this.y += this.velocity;
+        break;
+      case MovingDirection.left:
+        this.x -= this.velocity;
+        break;
+      case MovingDirection.right:
+        this.x += this.velocity;
+        break;
+    }
   }
 }
