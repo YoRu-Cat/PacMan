59b84271c9f0b8252415437c4367feb415ff72a8
YoRu Tech <tech.niko.coding@gmail.com>
2024-03-09 09:28:03 +0500

    Glich Fixes

--- src/Pacman.js
+++ src/Pacman.js
@@ -47,16 +47,16 @@ export default class Pacman {
   }
   #loadPacmanImages() {
     const pacmanImage1 = new Image();
-    pacmanImage1.src = "../images/pac0.png";
+    pacmanImage1.src = "images/pac0.png";
 
     const pacmanImage2 = new Image();
-    pacmanImage2.src = "../images/pac1.png";
+    pacmanImage2.src = "images/pac1.png";
 
     const pacmanImage3 = new Image();
-    pacmanImage3.src = "../images/pac2.png";
+    pacmanImage3.src = "images/pac2.png";
 
     const pacmanImage4 = new Image();
-    pacmanImage4.src = "../images/pac1.png";
+    pacmanImage4.src = "images/pac1.png";
 
     this.pacmanImages = [
       pacmanImage1,
